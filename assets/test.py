9cf993b6c551fcb63a6f3141d6b03e490f89d08f
ReDay Zarra <redayzarra@gmail.com>
2023-05-24 09:19:45 -0400

    Trying out OpenCV on model photo, it was a fail

---
+++ assets/test.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+
+face_data = cv.CascadeClassifier(
+    "C:\\Users\\reday\\Documents\\Drone-Project\\assets\\haarcascade_frontalface_default.xml"
+)
+eye_data = cv.CascadeClassifier(
+    "C:\\Users\\reday\\Documents\\Drone-Project\\assets\\haarcascade_eye.xml"
+)
+
+image = cv.imread("C:\\Users\\reday\\Documents\\Drone-Project\\assets\\face.jpg")
+
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+face = face_data.detectMultiScale(gray, 1.3, 5)
+print(len(face))
+
+for x, y, w, h in face:
+    cv.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    eye_gray = gray[y : y + h, x : x + w]
+    eye_color = image[y : y + h, x : x + w]
+    eyes = eye_data.detectMultiScale(eye_gray)
+    for ex, ey, ew, eh in eyes:
+        cv.rectangle(eye_color, (ex, ey), (ey + ew, ey + eh), (0, 255, 0), 2)
+
+cv.imshow("image", image)
+cv.waitKey(0)
+
+cv.destroyAllWindows()
