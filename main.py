96cfca1b1a89ec68bf0839a214fdd4ad7133e694
ReDay Zarra <redayzarra@gmail.com>
2023-05-29 06:05:43 -0400

    Adding json files to store all variables and starting init method

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-from logger import *
+import json
+import os
 import socket
 import sys
-import os
 import time
+import logging
 
+from logger import initialize_logging
+
+# Initialize logging
 initialize_logging()
+
+
+class Drone(object):
+    """
+    Class representing a drone. This class manages the communication
+    with the drone, including initializing the connection and sending
+    commands to the drone.
+    """
+
+    def __init__(self, config_file):
+        """
+        Initialize a Drone instance using the provided configuration file.
+
+        Args:
+            config_file (str): Path to the JSON configuration file.
+        """
+        try:
+            with open(config_file, "r") as f:
+                config = json.load(f)
+        except FileNotFoundError:
+            logging.error(f"Config file {config_file} not found.")
+            raise
+        except json.JSONDecodeError:
+            logging.error(f"Config file {config_file} has invalid JSON.")
+            raise
+
