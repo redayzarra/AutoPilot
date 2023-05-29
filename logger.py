e0c78ec486fe610993c9f97978d1d09b82dcf477
ReDay Zarra <redayzarra@gmail.com>
2023-05-29 05:44:01 -0400

    Adding logger file and initializing logger for main.py

---
+++ logger.py
@@ -0,0 +1,28 @@
+import logging
+import os
+from datetime import datetime
+
+
+def initialize_logging(log_level=logging.INFO, log_folder="logs"):
+    # Create a logs directory if it doesn't already exist
+    log_path = os.path.join(".", log_folder)
+    os.makedirs(log_path, exist_ok=True)
+    print(f"Directory '{log_path}' created or already exists.")
+
+    # Configure logging
+    log_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S.log")
+    log_file = os.path.join(log_path, log_name)
+
+    logging.basicConfig(
+        filename=log_file,
+        format="%(asctime)s - %(levelname)s: %(message)s",
+        encoding="utf-8",
+        level=log_level,
+    )
+
+    # Create a stream handler
+    ch = logging.StreamHandler()
+    ch.setLevel(log_level)
+
+    # Get the root logger and add the stream handler to it
+    logging.getLogger().addHandler(ch)
---
+++ main.py
@@ -0,0 +1,7 @@
+from logger import *
+import socket
+import sys
+import os
+import time
+
+initialize_logging()
