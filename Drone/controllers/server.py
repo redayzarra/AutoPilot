256f801676e1c18d4cd0bc528f6717766290d5c9
ReDay Zarra <redayzarra@gmail.com>
2023-06-06 18:08:23 -0400

    Adding all drone functions to server.py

--- Drone/controllers/server.py
+++ Drone/controllers/server.py
@@ -1,16 +1,82 @@
-from tools.logger import initialize_logging
+import os
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 from tools.config import create_app
 
-app = create_app()
+from ..models.Drone import Drone
 
-# The logging has already been set up in the create_app function.
+app = create_app()
+CORS(app)  # Enable CORS
+
+# The logging has already been set up
 app.logger.info("Server started.")
 
+# Initialize drone using config
+config_path = os.path.join("..", "..", "config.json")
+myDrone = Drone(config_path)
 
-@app.route("/")
-def index():
-    return "New Flask Project"
+
+@app.route("/drone/takeoff", methods=["POST"])
+def takeoff():
+    response = myDrone.takeoff()
+    return jsonify({"response": response})
 
 
-def run():
-    app.run(host=app.config["WEB_ADDRESS"], port=app.config["WEB_PORT"], threaded=True)
+@app.route("/drone/land", methods=["POST"])
+def land():
+    response = myDrone.land()
+    return jsonify({"response": response})
+
+
+@app.route("/drone/move", methods=["POST"])
+def move():
+    data = request.get_json()
+    direction = data.get("direction")
+    distance = data.get("distance")
+    response = myDrone.move_in_direction(direction, distance)
+    return jsonify({"response": response})
+
+
+@app.route("/drone/rotate", methods=["POST"])
+def rotate():
+    data = request.get_json()
+    direction = data.get("direction")
+    degree = data.get("degree")
+    response = myDrone.rotate(direction, degree)
+    return jsonify({"response": response})
+
+
+@app.route("/drone/flip", methods=["POST"])
+def flip():
+    data = request.get_json()
+    direction = data.get("direction")
+    response = myDrone.flip(direction)
+    return jsonify({"response": response})
+
+
+@app.route("/drone/patrol", methods=["POST"])
+def patrol():
+    myDrone.patrol()
+    return jsonify({"response": "Drone started patrolling"})
+
+
+@app.route("/drone/stop_patrol", methods=["POST"])
+def stop_patrol():
+    myDrone.stop_patrol()
+    return jsonify({"response": "Drone stopped patrolling"})
+
+
+@app.route("/drone/stop", methods=["POST"])
+def stop():
+    myDrone.stop()
+    return jsonify({"response": "Drone stopped"})
+
+
+if __name__ == "__main__":
+    try:
+        app.run(host="0.0.0.0", port=5000)
+    except Exception as e:
+        myDrone.stop()
+        print(f"An error occurred: {e}")
