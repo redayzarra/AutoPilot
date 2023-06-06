39dcc042278d7691f269e7f424addf8b394ab67e
ReDay Zarra <redayzarra@gmail.com>
2023-06-06 15:33:10 -0400

    Getting ready to add react app to project

--- tools/main.py
+++ tools/main.py
@@ -2,7 +2,7 @@ import Drone.controllers.server
 from tools.logger import initialize_logging
 
 # Initialize logging
-initialize_logging(subfolder="MainLogs")
+initialize_logging()
 
 if __name__ == "__main__":
     Drone.controllers.server.run()
