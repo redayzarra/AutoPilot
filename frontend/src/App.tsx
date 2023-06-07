4f3e03cc8fb3dc1baf0e964c6346f5364fa7636e
ReDay Zarra <redayzarra@gmail.com>
2023-06-07 07:59:51 -0400

    Getting ready to build the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,9 @@
-import { useEffect, useRef } from "react";
-import "./App.css";
+import Message from "./components/Message";
 
 function App() {
-  const ref = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    if (ref.current) ref.current.focus();
-  });
-
   return (
     <div>
-      <input ref={ref} type="text" className="form-control" />
+      <Message />
     </div>
   );
 }
---
+++ frontend/src/components/Message.tsx
@@ -0,0 +1,8 @@
+
+const Message = () => {
+  return (
+    <h1>Another hello world</h1>
+  )
+}
+
+export default Message
