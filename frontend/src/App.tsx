fddcdee122464b4a242279115470d353db30450a
ReDay Zarra <redayzarra@gmail.com>
2023-06-08 07:24:03 -0400

    Building grid layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
-import Test from "./components/Test";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Test />;
+  return (
+    <Grid
+      templateAreas={`"nav nav nav" 
+                      "flip camera height"`}
+    >
+      <GridItem area="nav" bg="coral">
+        NavBar
+      </GridItem>
+      <GridItem area="flip" bg="darkgreen">
+        Flips
+      </GridItem>
+      <GridItem area="camera" bg="gold">
+        Camera
+      </GridItem>
+      <GridItem area="height" bg="green">
+        Height
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
