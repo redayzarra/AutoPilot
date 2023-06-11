93581e5d268612277fa79f08fefba31dbd3327de
ReDay Zarra <redayzarra@gmail.com>
2023-06-11 10:15:48 -0400

    Wrapping up frontend

--- frontend/src/components/StopButton.tsx
+++ frontend/src/components/StopButton.tsx
@@ -2,7 +2,15 @@ import DroneButton from "./DroneButtons";
 import { GoStop } from "react-icons/go";
 
 const StopButton = () => {
-  return <DroneButton size="md" colorScheme="red" text="Stop" leftIcon={<GoStop />} />;
+  return (
+    <DroneButton
+      size="md"
+      colorScheme="red"
+      text="Stop"
+      leftIcon={<GoStop />}
+      marginTop="-50px"
+    />
+  );
 };
 
 export default StopButton;
