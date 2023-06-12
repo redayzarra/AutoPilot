cc150224030bfea0612e42a79dcf9715e967dbc8
ReDay Zarra <redayzarra@gmail.com>
2023-06-12 05:57:19 -0400

    Decided to use React Query with Axios to connect data

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import theme from "./theme.ts";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
