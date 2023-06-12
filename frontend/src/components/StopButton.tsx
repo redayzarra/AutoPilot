349ee67f6f8ae84fdcb2e88a3aaf98d65967ad78
ReDay Zarra <redayzarra@gmail.com>
2023-06-12 10:32:17 -0400

    Connection StopButton to the backend

--- frontend/src/components/StopButton.tsx
+++ frontend/src/components/StopButton.tsx
@@ -1,7 +1,17 @@
+import axios from "axios";
 import DroneButton from "./DroneButtons";
 import { GoStop } from "react-icons/go";
 
 const StopButton = () => {
+  const handleStop = async () => {
+    try {
+      const response = await axios.post("http://localhost:5000/drone/stop");
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <DroneButton
       size="md"
@@ -9,6 +19,7 @@ const StopButton = () => {
       text="Stop"
       leftIcon={<GoStop />}
       marginTop="-50px"
+      onClick={handleStop}
     />
   );
 };
